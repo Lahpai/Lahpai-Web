9f6005b9187f12bfe065b05157f914156f3cb969
San <lahpaimufc@gmail.com>
2022-09-28 00:29:47 -0500

    CookieSession

---
+++ CookieSession/q3/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const session = require('express-session');
+app.use(session ({
+    secret: "Big Secret"
+}));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'view'));
+app.use('/css', express.static(path.join(__dirname, 'css')));
+
+const Product = require('./models/product');
+
+app.get("/", (req, res) => {
+    let pList = Product.getAllProducts();
+    res.render('product', { products : pList});
+});
+
+
+app.get("/shoppingcart",(req, res) =>{
+    // let products=Product.getCart();
+    let products= req.session.cart;  
+     res.render("shoppingcart", {products}
+     )
+ });
+
+
+app.listen(3000);
